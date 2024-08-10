8a0250629bc36f135da6c4943835348053d69f51
ncs-4-u <nimbus4consultancy@gmail.com>
2024-08-10 23:40:35 +0200

    newfiles

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.about, name='about')
+    path('about/', views.about, name='about')
 ]
