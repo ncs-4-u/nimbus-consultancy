4780bb16af405b7c3eed0a4d30c287c78207971b
ncs-4-u <nimbus4consultancy@gmail.com>
2024-08-12 01:50:48 +0200

    asd

--- static/index.js
+++ static/index.js
@@ -2,7 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById("hamburger_icon");
     const navLinks = document.getElementById("navLinks");
 
-    hamburger.addEventListener("click", function() {
-        navLinks.classList.toggle("show");
-    });
+    // Check if elements exist
+    if (hamburger_icon && navLinks) {
+        hamburger_icon.addEventListener("click", function() {
+            navLinks.classList.toggle("show");
+        });
+    } else {
+        console.error("Hamburger or navLinks element not found in the DOM.");
+    }
 });
