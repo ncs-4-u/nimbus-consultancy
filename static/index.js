5e8edb31141f0900106f2baa12fef934ca2a2686
ncs-4-u <nimbus4consultancy@gmail.com>
2024-08-12 01:33:31 +0200

    ad

--- static/index.js
+++ static/index.js
@@ -1,31 +1,8 @@
-var counter = 1;
-setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 4) {
-        counter = 1;
-    }
-}, 2000);
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById("hamburger_icon");
+    const navLinks = document.getElementById("navLinks");
 
-// console.log("start");
-document.getElementById("hamburger_icon").addEventListener('onclick', show_responsive_box);
-// console.log("event_listner");
-var o = document.getElementById("nav_responsive_box");
-// console.log("var box acquired");
-function show_responsive_box() {
-    // console.log("inside function");
-    if (o.style.display != "flex") {
-        // console.log("inside if");
-        o.style.display = "flex";
-    }
-    else { o.style.display = "none" }
-}
-
-document.getElementById("close_responsive_box").addEventListener('onclick',hide_responsive_box);
-var x = document.getElementById("nav_responsive_box");
-function hide_responsive_box(){
-    if(x.style.display != "none"){
-        x.style.display = "none";
-    }
-
-}
+    hamburger.addEventListener("click", function() {
+        navLinks.classList.toggle("show");
+    });
+});
